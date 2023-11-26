040d8b76e555eef44887089f9af165f17852448c
RH Sadik <sadikcsbd@gmail.com>
2023-11-26 17:21:34 +0600

    weather app loading...

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation></SiteNavigation>
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router"
+import SiteNavigation from "./components/SiteNavigation.vue"
+</script>
+
+<style lang="scss" scoped></style>
